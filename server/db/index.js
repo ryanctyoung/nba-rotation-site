840dbc7b20a32d4ed165b8946a13d1b968fab55e
Ryan Young <lightning16ry@gmail.com>
2025-02-23 01:56:17 -0800

    Update Port Environment Variable usage

--- server/db/index.js
+++ server/db/index.js
@@ -11,6 +11,7 @@ export const AppDataSource = new DataSource({
   type: "postgres",
   host: process.env.DB_HOST,
   username: process.env.DB_USER,
+  port: process.env.DB_PORT,
   password: process.env.DB_PASS,
   database: process.env.DB_USER,
   entities: [
